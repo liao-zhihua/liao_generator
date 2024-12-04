083d75fe78d96cc947783fda5532e2ec01d0b6b0
liaozhihua <liaozhihua1688@gmail.com>
2024-12-04 23:08:57 +0800

    静态和动态模板生成

---
+++ liao_generator_basic/src/main/java/com/zhihua/model/MainTemplateConfig.java
@@ -0,0 +1,27 @@
+package com.zhihua.model;
+
+import lombok.Data;
+
+/**
+ * 动态模版配置
+ */
+@Data
+public class MainTemplateConfig {
+
+    /**
+     * 是否生成循环
+     */
+    private boolean loop = false;
+
+    /**
+     * 作者注释
+     */
+    private String author = "Liao zhihua";
+
+    /**
+     * 输出信息
+     */
+    private String outputText = "sum = ";
+}
+
+
